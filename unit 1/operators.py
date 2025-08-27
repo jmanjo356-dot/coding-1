c76850bb07995c85fd04c87dc103e9964ea94bbc
jmanjo356-dot <jmanjo356@boyslatin.net>
2025-08-27 14:27:18 +0000

    first commit

---
+++ unit 1/operators.py
@@ -0,0 +1,31 @@
+# operators
+# operators are a construct (built in systems) that give
+# data types more actions 
+
+# Arithmetic operator family 
+# Arihmatic operators are simply math operations 
+
+print(2+4) # Addition operator
+print(10-10) # Subtraction operator 
+print(14.8/20) # Division operator
+print(72.4+45.23) # multiplacation operator
+# print is a function wherre anything writen in the round braket 
+
+# assignment operator family
+# these operators assign values to
+# variables (otherwise known as containers)
+# key / value
+
+SchoolName = "Boys latin"
+Age = 16
+password= "OjSImpson" 
+username= 77329
+search = "baconator fries"
+
+
+Age+=2  #addign a new value to our variable 
+Age*=14  # multiplying a new value to our variable 
+
+
+print(SchoolName)
+print(Age)
