926f00ab6b4db9e51785cefe79551b9029ab7e4a
jmanjo356-dot <jmanjo356@boyslatin.net>
2025-09-11 13:57:30 +0000

    blab;labla

---
+++ unit 1/airhandeling.py
@@ -0,0 +1,31 @@
+# type error = (data typre error)
+# the computer has detected an issue with how you are useing 
+# data types
+
+print ('coding class is in class ' + str(105))
+print(int('100') + 100)
+
+
+# syntax error = the computer has detectred an
+# issue with how your code is written
+# somewhere you btoke the grammer rules of a langueg 
+
+ #cant start with a number
+
+book10= 10
+print(1 + book10)
+
+print('have a great day ')
+
+# 
+
+# Arithmatic error= the computer had detected a math issue 
+
+print(100/.083)
+
+# indetentation error = the computer has detected 
+#an issue with a function specifically how the function
+# is spread 
+
+def function():
+    print('have a good day of classes, and push in your chairs')
---
+++ unit 2 /functions.py
@@ -0,0 +1,38 @@
+# function is aset of instructions labeld 
+# under a custom name, that a computer will run
+
+
+# function syntax (rules of how its written)
+# functio have 2 phase: function definition and function call
+
+# function definition
+# we are describing the instructions for our custom code
+# we are adding logic to the computers vocabulary
+# this code does not run, it only gives the computer the meaning
+# not action
+
+def example():
+    print('mohamed is ugly') # 1 instruction :print mohamed is ugly
+
+# phase 2: function call
+# once we have the definition, we can now run the instruction
+# by writting the function name
+
+example()
+
+# we indent to inform the computer
+# that we are about to write instrutions
+# if we dont, we will get an error
+def example2():
+    print('good boy')
+    a= input('enter a number ')
+    print(a)
+
+example2()  
+
+def math():
+    a = input('please enter a number: ')
+    b = 20
+    print('what is your result! ')
+    print(int(a) + b)
+
